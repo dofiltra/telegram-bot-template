5b02dc5c656a1ea165b5075c1ac95924cadd524e
memanot <menemty@gmail.com>
2021-09-23 14:29:29 +0300

    rootPath

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
+import path from 'path'
+import dotenv from 'dotenv'
+import { fileURLToPath } from 'url'
 import { TG } from './services/tg'
 
 export class App {
   static version = 1
+  static rootPath = path.join(path.dirname(fileURLToPath(import.meta.url)), '..')
 
-  start(tokens?: string) {
+  constructor() {
+    dotenv.config({ path: `${App.rootPath}/.env` })
+  }
+
+  async start(tokens?: string) {
     if (!tokens) {
       return console.error(`Required 'TOKENS' in '.env'`)
     }
--- src/services/tg.ts
+++ src/services/tg.ts
@@ -13,7 +13,29 @@ class TG {
     this.bot = new Telegraf(s.token)
   }
 
-  async start() {}
+  async start() {
+    const bot = this.bot
+
+    // // Middlewares
+    // bot.use(ignoreOldMessageUpdates)
+    // bot.use(attachUser)
+    // bot.use(i18n.middleware(), attachI18N)
+
+    // // Commands
+    // bot.command(['help', 'start'], sendHelp)
+    // bot.command('language', sendLanguage)
+
+    // // Actions
+    // bot.action(localeActions, setLanguage)
+
+    // // Errors
+    // bot.catch(console.error)
+
+    // // Start bot
+    // bot.launch().then(() => {
+    //   console.info(`Bot '${bot.botInfo.username}' is up and running`)
+    // })
+  }
 }
 
 export { TG }
